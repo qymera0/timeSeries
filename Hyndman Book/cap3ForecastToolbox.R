7de93a5d3d3263c4e261bc58e7fb2a8d98386386
Samuel B Baco <samuelbaco@gmail.com>
2020-09-15 10:34:04 -0300

    cap3

--- Hyndman Book/cap3ForecastToolbox.R
+++ Hyndman Book/cap3ForecastToolbox.R
@@ -1,4 +1,6 @@
-
+library(tidyverse)
+library(fpp)
+library(fpp2)
 
 # 3.1 SIMPLE FORECASTING METHOD -------------------------------------------
 
@@ -110,4 +112,119 @@ Box.test(res, lag = 10, fitdf = 0)
 Box.test(res,lag=10, fitdf=0, type="Lj")
 
 checkresiduals(naive(goog200))
+
+# 3.4 EVALUATING FORECAST -------------------------------------------------
+
+# subset time series
+
+window(ausbeer, start = 1995)
+
+subset(ausbeer, start = length(ausbeer)-4*5) # last five years
+
+subset(ausbeer, quarter = 1)
+
+tail(ausbeer, 4*5)
+
+# Forecast error
+
+# Use only a part of the data to fit
+
+beer2 <- window(ausbeer,start=1992,end=c(2007,4))  
+
+beerfit1 <- meanf(beer2,h=10)
+
+beerfit2 <- rwf(beer2,h=10)
+
+beerfit3 <- snaive(beer2,h=10)
+
+autoplot(window(ausbeer, start=1992)) +
+        autolayer(beerfit1, series="Mean", PI=FALSE) +
+        autolayer(beerfit2, series="Naïve", PI=FALSE) +
+        autolayer(beerfit3, series="Seasonal naïve", PI=FALSE) +
+        xlab("Year") + ylab("Megalitres") +
+        ggtitle("Forecasts for quarterly beer production") +
+        guides(colour=guide_legend(title="Forecast"))
+
+# use the other part to test
+
+beer3 <- window(ausbeer, start=2008)
+
+accuracy(beerfit1, beer3)
+
+accuracy(beerfit2, beer3)
+
+accuracy(beerfit3, beer3)
+
+# Google example
+
+googfc1 <- meanf(goog200, h=40)
+
+googfc2 <- rwf(goog200, h=40)
+
+googfc3 <- rwf(goog200, drift=TRUE, h=40)
+
+autoplot(subset(goog, end = 240)) +
+        autolayer(googfc1, PI=FALSE, series="Mean") +
+        autolayer(googfc2, PI=FALSE, series="Naïve") +
+        autolayer(googfc3, PI=FALSE, series="Drift") +
+        xlab("Day") + ylab("Closing Price (US$)") +
+        ggtitle("Google stock price (daily ending 6 Dec 13)") +
+        guides(colour=guide_legend(title="Forecast"))
+
+googtest <- window(goog, start=201, end=240)
+
+accuracy(googfc1, googtest)
+
+accuracy(googfc2, googtest)
+
+accuracy(googfc3, googtest)
+
+# Cross validation
+
+e <- tsCV(goog200, rwf, drift = TRUE, h = 1)
+
+sqrt(mean(e^2, na.rm = TRUE))
+
+sqrt(mean(residuals(rwf(goog200, drift=TRUE))^2, na.rm=TRUE))
+
+# Pipe operation
+
+goog200 %>% 
+        tsCV(forecastfunction = rwf,
+             drift = T,
+             h = 1) -> e
+
+e^2 %>% 
+        mean(na.rm = T) %>% 
+        sqrt()
+
+
+# Other example
+
+e <- tsCV(goog200, forecastfunction=naive, h=8)
+
+# Compute the MSE values and remove missing values
+
+mse <- colMeans(e^2, na.rm = T)
+
+# Plot the MSE values against the forecast horizon
+
+data.frame(h = 1:8, MSE = mse) %>%
+        ggplot(aes(x = h, y = MSE)) + geom_point()
+
+# 3. PREDICTION INTERVALS -------------------------------------------------
+
+naive(goog200)
+
+autoplot(naive(goog200))
+
+# bootstrap - several simulations of future using the past
+
+naive(goog200, bootstrap=TRUE)
+
+autoplot(naive(goog200, bootstrap = T))
+
+
+# 3.6 FORECAST PACKAGE ----------------------------------------------------
+
 
